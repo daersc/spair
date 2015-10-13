3ed8d5c5f04e9bf372143753eef4b01ccc2b5a1d
David Schury <dasc@posteo.de>
2015-10-13 13:53:07 +0200

    Added access to roundrobin generator via '-r' opt.
    When passing -r to spair, it will read a list of names line by line from STDIN
    and generate pairings via roundrobin algorithm.

--- spair.c
+++ spair.c
@@ -4,6 +4,7 @@
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #ifdef USING_GETTEXT
@@ -11,12 +12,16 @@
 #endif
 #include "gettext.h"
 
-#define CL_OPT_STRING ":hv"
+#include "roundrobin.h"
+
+#define MAX_NAME_LEN  0xff /* 255 */
+#define CL_OPT_STRING ":hvr"
 
 static void init_locale(void);
 static void parse_args(int argc, char **argv);
 static void print_version(void);
 static void print_help(void);
+static void pair_roundrobin(void);
 
 static void
 init_locale(void)
@@ -42,6 +47,9 @@ parse_args(int argc, char **argv)
 		case 'v':
 			print_version();
 			exit(EXIT_SUCCESS);
+		case 'r':
+			pair_roundrobin();
+			break;
 		case ':':
 			fprintf(stderr, _("Option -%c requires an argument\n"), optopt);
 			exit(EXIT_FAILURE);
@@ -63,7 +71,68 @@ print_version(void)
 static void
 print_help(void)
 {
-	printf(_("Usage: %s\n"), "spair [-v] [-h]");
+	const char *fmt = " -%c  %s\n";
+	printf(_("Usage: %s\n"), "spair [-v] [-h] [-r]");
+	printf(fmt, 'h', _("Print this help and exit."));
+	printf(fmt, 'v', _("Print version information and exit."));
+	printf(fmt, 'r', _("Read list of players from STDIN and "
+	                   "print roundobin pairings."));
+}
+
+static void
+pair_roundrobin(void)
+{
+	Player *list = NULL, *lptr = NULL;
+	Round  *hist = NULL, *rptr = NULL;
+	char   *buffer = NULL;
+	size_t buflen  = 0;
+	unsigned int i, j;
+	while (getline(&buffer, &buflen, stdin) != -1) {
+		Player *player = malloc(sizeof(Player));
+		size_t pos;
+		for (pos = 0; pos < buflen; pos++) {
+			if (buffer[pos] == '\n') {
+				buffer[pos] = '\0';
+				break;
+			}
+		}
+		if (!player) {
+			perror(_("Could not allocate memory for player"));
+			goto cleanup;
+		}
+		player->next = NULL;
+		player->name = malloc(sizeof(char) * (strlen(buffer)+1));
+		if (!player->name) {
+			perror(_("Could not allocate memory for player's name"));
+			free(player);
+			goto cleanup;
+		}
+		strcpy(player->name, buffer);
+		if (lptr) lptr->next = player;
+		else list = player;
+		lptr = player;
+	}
+
+	hist = rptr = roundrobin(list);
+	i = 1;
+	while (rptr) {
+		Pairing *pptr = rptr->pairings;
+		j=1;
+		printf(i==1 ? "%s %u\n" : "\n%s %u\n", _("Round"), i);
+		while (pptr) {
+			printf(" %20s : %-20s\n", pptr->white ? pptr->white->name : _("<bye>"),
+			                          pptr->black ? pptr->black->name : _("<bye>"));
+			pptr = pptr->next;
+			j++;
+		}
+		rptr = rptr->next;
+		i++;
+	}
+
+	cleanup:
+	free(buffer);
+	free_players(list);
+	free_rounds(hist);
 }
 
 int
